cfc31aba29d77eb37a5f1deaee4e55abe9171531
Aristides Staffieri <aristides@aristidesstaffieri.com>
2015-02-22 18:24:52 -0700

    added directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,91 +20,13 @@
    $scope.description = '';
    $scope.wind = 20;
 
-
-
-   var graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
-
-   var margin = {top: 0, right: 0, bottom: 0, left: 0};
-   var w = 400 - margin.left - margin.right,
-   h = 300 - margin.top - margin.bottom;
-
-   var svg = d3.select('#bar-graph').append('svg')
-   .attr('width', w + margin.left + margin.right)
-   .attr('height', h + margin.top + margin.bottom)
-   .append('g')
-   .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-
-   var yScale = d3.scale.linear()
-   .domain([0, d3.max(graphData)])
-   .range([0, h]);
-
-   var colorScale = d3.scale.quantize()
-   .domain([0, graphData.length])
-   .range(['orange', 'purple', 'red', 'green', 'black']);
-
-   var xScale = d3.scale.ordinal()
-   .domain(graphData)
-   .rangeBands([0, w], 0.1, 0);
-
-   svg.selectAll('rect')
-   .data(graphData)
-   .enter()
-   .append('rect')
-   .attr('class', 'bar')
-   .attr('x', function (d) {
-    return xScale(d);
-  })
-   .attr('y', function (d) {
-    return h - yScale(d);
-  })
-   .attr('width', xScale.rangeBand())
-   .attr('height', function (d) {
-    return yScale(d);
-  })
-   .attr('fill', function (d, i) {
-    return colorScale(i);
-  });
-
-
+   $scope.graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
+   
    $scope.fakeWeather = function () {
-    graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
+    $scope.graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
+  };
 
-    yScale = d3.scale.linear()
-   .domain([0, d3.max(graphData)])
-   .range([0, h]);
-
-   colorScale = d3.scale.quantize()
-   .domain([0, graphData.length])
-   .range(['orange', 'purple', 'red', 'green']);
-
-   xScale = d3.scale.ordinal()
-   .domain(graphData)
-   .rangeBands([0, w], 0.1, 0);
-
-     svg.selectAll('rect')
-     .data(graphData)
-     .transition()
-     .duration(250)
-     .delay(function (d, i) {
-      return i * 25;
-     })
-     .transition()
-     .duration(250)
-     .attr('x', function (d) {
-      return xScale(d);
-    })
-     .transition()
-     .duration(500)
-     .attr('y', function (d) {
-      return h - yScale(d);
-    })
-     .attr('width', xScale.rangeBand())
-     .attr('height', function (d) {
-      return yScale(d);
-    });
-   };
-
-   $scope.searchWeather = function () {
+  $scope.searchWeather = function () {
     WeatherDataService.getWeather($scope.searchTerm)
     .then(function(data) {
      $scope.name = data.data.name;
@@ -114,43 +36,7 @@
      $scope.description = data.data.weather[0].description;
      $scope.wind = data.data.wind.speed;
 
-     graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
-
-     yScale = d3.scale.linear()
-   .domain([0, d3.max(graphData)])
-   .range([0, h]);
-
-   colorScale = d3.scale.quantize()
-   .domain([0, graphData.length])
-   .range(['orange', 'purple', 'red', 'green']);
-
-   xScale = d3.scale.ordinal()
-   .domain(graphData)
-   .rangeBands([0, w], 0.1, 0);
-
-     svg.selectAll('rect')
-     .data(graphData)
-     .transition()
-     .duration(250)
-     .delay(function (d, i) {
-      return i * 25;
-     })
-     .transition()
-     .duration(250)
-     .attr('x', function (d) {
-      return xScale(d);
-    })
-     .transition()
-     .duration(500)
-     .attr('y', function (d) {
-      return h - yScale(d);
-    })
-     .attr('width', xScale.rangeBand())
-     .attr('height', function (d) {
-      return yScale(d);
-    });
-   });
-  };
-
-
+     $scope.graphData = [$scope.temp,  $scope.humidity, $scope.pressure, $scope.wind];
+       });
+    };
 });
---
+++ app/scripts/directives/my-d3-graph.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularD3App.controller:D3 Bar Graph
+ * @description
+ * # D3 Bar Graph
+ * Controller of the angularD3App
+ */
+angular.module('angularD3App')
+.directive('myD3Graph', function () {
+	//private stuff
+	var margin = {top: 0, right: 0, bottom: 0, left: 0};
+	var w = 400 - margin.left - margin.right,
+	h = 300 - margin.top - margin.bottom;
+
+ 	return {
+ 		restrict: 'E',
+ 		replace: true,
+ 		scope: { //binds scope
+ 			val: '='
+ 		},
+ 		template: '<div id="bar-graph"></div>',
+ 		link: function (scope, element, attrs) {
+
+ 			//svg obj
+ 			var svg = d3.select('#bar-graph').append('svg')
+ 			.attr('width', w + margin.left + margin.right)
+ 			.attr('height', h + margin.top + margin.bottom)
+ 			.append('g')
+ 			.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+ 			var yScale = d3.scale.linear()
+ 			.domain([0, d3.max(scope.$parent.graphData)])
+ 			.range([0, h]);
+
+ 			var colorScale = d3.scale.quantize()
+ 			.domain([0, scope.$parent.graphData.length])
+ 			.range(['orange', 'purple', 'red', 'green']);
+
+ 			var xScale = d3.scale.ordinal()
+ 			.domain(scope.$parent.graphData)
+ 			.rangeBands([0, w], 0.1, 0);
+
+ 			svg.selectAll('rect')
+ 			.data(scope.$parent.graphData)
+ 			.enter()
+ 			.append('rect')
+ 			.attr('class', 'bar')
+ 			.attr('x', function (d) {
+ 				return xScale(d);
+ 			})
+ 			.attr('y', function (d) {
+ 				return h - yScale(d);
+ 			})
+ 			.attr('width', xScale.rangeBand())
+ 			.attr('height', function (d) {
+ 				return yScale(d);
+ 			})
+ 			.attr('fill', function (d, i) {
+ 				return colorScale(i);
+ 			});
+
+ 			scope.$watch('val', function(newVal, oldVal) {
+
+ 				yScale = d3.scale.linear()
+ 				.domain([0, d3.max(newVal)])
+ 				.range([0, h]);
+ 				svg.selectAll('rect')
+ 				.data(newVal)
+ 				.transition()
+ 				.duration(250)
+ 				.delay(function (d, i) {
+ 					return i * 25;
+ 				})
+ 				.transition()
+ 				.duration(250)
+ 				.transition()
+ 				.duration(500)
+ 				.attr('y', function (d) {
+ 					return h - yScale(d);
+ 				})
+ 				.attr('width', xScale.rangeBand())
+ 				.attr('height', function (d) {
+ 					return yScale(d);
+ 				});
+ 			});
+ 		}
+ 	};
+ });
